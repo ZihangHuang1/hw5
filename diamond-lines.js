15ca7cf8ec9cd9dca996776cce633f5e41611006
ZihangHuang1 <47286210+ZihangHuang1@users.noreply.github.com>
2019-03-02 15:49:16 -0800

    Create diamond-lines.js

---
+++ diamond-lines.js
@@ -0,0 +1,14 @@
+    function setup() {
+    	createCanvas(400, 400);
+    }
+
+    function draw() {
+    	background(400);
+    	for (var i = 10; i <= width / 2; i = i + 10) {
+    		line(200 - i, i, 200 + i, i);
+    	}
+    	for (var i = 10; i <= height; i = i + 10) {
+    		line(i, height / 2 + i, 400 - i, height / 2 + i);
+    	}
+
+    }
